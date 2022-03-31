340ec5bba7f4f079fbf9c6f31c6a1b8613dcecf3
Anton <antonrozdobudko@gmail.com>
2022-03-31 21:33:56 +1000

    Codewars. 6kyu level. Split Strings.

--- main.js
+++ main.js
@@ -1,35 +1,28 @@
 // 6kyu level
-// https://www.codewars.com/kata/578aa45ee9fd15ff4600090d
+// https://www.codewars.com/kata/515de9ae9dcfc28eb6000001
 
 // Description
-// #Sort the odd
+// #Split Strings
 
 // Task
-// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
 //
-//     Examples
-//     [7, 1]  =>  [1, 7]
-//     [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
-//     [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+//     Examples:
+//
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
 
-function sortArray(array) {
-    let oddArray = array.filter(f => f%2 !== 0)
-    let sortedArray = []
-    for (let i = 0; i < oddArray.length; i++) {
-        sortedArray.push(oddArray.reduce((acc, el) => acc < el ? acc : el))
-        delete oddArray[oddArray.indexOf(oddArray.reduce((acc, el) => acc < el ? acc : el))]
+function solution(str) {
+    let arrayFinal = []
+    let count = str.length
+    let temp = 0
+    while (count >= 2) {
+        arrayFinal.push(str.substr(temp, 2))
+        temp += 2
+        count -= 2
     }
-    let finalArray = []
-    let order = 0
-    for (let i = 0; i < array.length; i++) {
-        if (array[i]%2 !== 0) {
-            finalArray.push(sortedArray[order])
-            order++
-        } else {
-            finalArray.push(array[i])
-        }
-    }
-    return finalArray
+    if (count === 1) arrayFinal.push(`${str.substr(temp, 1)}_`)
+    return arrayFinal
 }
 
-console.log(sortArray([5, 3, 2, 8, 1, 4]))
+console.log(solution(''))
