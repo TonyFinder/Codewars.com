5707251f139959e4d43048000be8802bb649e9a4
Anton <antonrozdobudko@gmail.com>
2022-05-13 23:15:00 +1000

    Task from someone's interview.

--- main.js
+++ main.js
@@ -1,39 +1,68 @@
 // 5kyu level
-// https://www.codewars.com/kata/525c65e51bf619685c000059/train/javascript
+// https://www.codewars.com/kata/514a024011ea4fb54200004b/train/javascript
 
 // Description
-// # Pete, the baker
+// # Extract the domain name from a URL
 
 // // Task
-// Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
 //
-//     Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
-//
-// Examples:
-//
-// // must return 2
-//     cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});
-// // must return 0
-// cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
+// * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+// * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+// * url = "https://www.cnet.com"                -> domain name = cnet"
 
-function cakes(recipe, available) {
-    let recipeUnits = Object.keys(recipe)
-    let availableUnits = Object.keys(available)
-    let indicator = 1
-    for (let i = 0; i < recipeUnits.length; i++) {
-        if (availableUnits.indexOf(recipeUnits[i]) < 0) {
-            indicator = 0
-            break
-        }
-    }
-    if (indicator === 0) return 0
+/*function domainName(url){
+    let num = url.indexOf('.')
 
-    let recipeCount = []
-    for (let i = 0; i < recipeUnits.length; i++) {
-        let temp = recipeUnits[i]
-        recipeCount.push((Math.floor(available[`${temp}`] / recipe[`${temp}`]) < 0) ? 0 : Math.floor(available[`${temp}`] / recipe[`${temp}`]))
-    }
-    return Math.min(...recipeCount)
+    return num
 }
 
-console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, milk: 200, eggs: 1}))
+console.log(domainName("http://google.com"))*/
+
+
+//Task from someone's interview!!!!!!!!!
+// Дан массив чисел, нужно вернуть строку интервалов из подряд идущих чисел.
+// Если подряд идут три и более числа с инкрементом 1, то они связываются "-"
+
+// const arr = [9, 2, 6, 5, 3, 8, 1, 10, 12, 15];
+// =>'1-3, 5, 6, 8-10, 12, 15'
+
+
+function intervals(arr) {
+    arr.sort((a, b) => a < b ? -1 : 1)
+    let tempVar = 1
+    let tempArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i + 1] === arr[i] + 1) {
+            tempVar++
+        } else {
+            tempArr.push(tempVar)
+            tempVar = 1
+        }
+    }
+
+    let numberArr = []
+    for (let i = 0; i < tempArr.length; i++) {
+        if (tempArr[i] > 2) {
+            numberArr.push(tempArr[i])
+        } else {
+            for (let j = 0; j < tempArr[i]; j++) {
+                numberArr.push(1)
+            }
+        }
+    }
+
+    let finalArr = []
+    for (let i = 0; i < numberArr.length; i++) {
+        if (numberArr[i] > 2) {
+            finalArr.push(`${arr[0]}-${arr[numberArr[i]-1]}`)
+            arr.splice(0, numberArr[i])
+        } else {
+            finalArr.push(arr[0])
+            arr.splice(0, numberArr[i])
+        }
+    }
+    return finalArr.join(', ')
+}
+
+console.log(intervals([9, 2, 6, 5, 3, 8, 1, 10, 12, 15]))
